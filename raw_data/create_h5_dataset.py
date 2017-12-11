8f1c21dbbe18b1d272cc2d95360324571723b3f2
ES2016_14353420 <977961978@qq.com>
2017-12-12 00:51:26 +0800

    将图像数据处理为数据库

--- raw_data/create_h5_dataset.py
+++ raw_data/create_h5_dataset.py
@@ -2,8 +2,7 @@ from tflearn.data_utils import build_hdf5_image_dataset
 import h5py
 
 
-
-path = '/home/smie/zhengjx/Res_Bilinear_cnns/raw_data/txt/'
+path = '/home/smie/zhengjx/Res_Bilinear_cnns/raw_data/txt/' 
 filenum = 50;
 filename = 'train_data'
 files = [];
